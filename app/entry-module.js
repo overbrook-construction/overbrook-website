cca8f3c566d44fcb3b393eb5bddd6e0b58709c6d
Sam Gruse <sgruse89@gmail.com>
2016-05-10 16:25:56 -0700

    Setting up angular and gulp

--- app/entry-module.js
+++ app/entry-module.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// const angular = require('angular');
+//
+// (function() {
+//   //INSERT MODULE NAMES IN ARRAY
+//   angular.module('App', ['RouteModule']);
+// })();
+
+
+
 const angular = require('angular');
 
 (function() {
