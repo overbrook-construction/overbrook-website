bdfbb697d219134c17733759949c94744402a786
Sam Gruse <sgruse89@gmail.com>
2016-05-12 13:04:25 -0700

    Set up google maps for our map page

--- app/components/map/map-controller.js
+++ app/components/map/map-controller.js
@@ -1,1 +1,17 @@
 'use strict';
+
+angular.module('MapModule', [])
+  .controller('MapController', function() {
+    this.initMap = function() {
+      var mapDiv = document.getElementById('map');
+      // console.log(new google.maps.Map);
+      var map = new google.maps.Map(mapDiv, {
+        center: {
+          lat: 47.629,
+          lng: -122.211},
+          zoom: 12
+      });
+    }
+  })
+  // lat: 47.629,
+  // lng: -122.211
--- app/entry.js
+++ app/entry.js
@@ -6,7 +6,7 @@ const angular = require('angular');
 require('./components/nav/nav-controller.js');
 require('./components/home/home-controller.js');
 require('./components/about/about-controller.js');
-// require('./components/map/map-controller.js');
+require('./components/map/map-controller.js');
 // require('./components/gallery/galler-controller.js');
 require('./components/contact/contact-controller.js');
 require('./components/footer/footer-controller.js')
