2ee31cdedce862393834bad6a26a2795bd59ac75
Sam Gruse <sgruse89@gmail.com>
2016-05-12 17:17:54 -0700

    Moved house data to JSON file and have our google maps being populated

--- public/bundle.js
+++ public/bundle.js
@@ -30985,29 +30985,25 @@
 
 /***/ },
 /* 6 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
-
+	const angular = __webpack_require__(1);
 
 	angular.module('MapModule', [])
-	  .controller('MapController', function() {
+	  .controller('MapController', ['$http', function($http) {
 	    var geotags = [];
-	    var markerData = [
-	      {
-	        address: "2432 Evergreen Point Road Medina, WA 98039"
-	      },
-	      {
-	        address: "2434 Evergreen Point Road Medina, WA 98039"
-	      },
-	      {
-	        address: "2436 Evergreen Point Road Medina, WA 98039"
-	      },
-	      {
-	        address: "7720 Overlake Drive W Medina, WA 98039"
-	      },
-	    ];
+	    var markerData;
+
+	    this.getData = function() {
+	      $http.get('./data/homes.json')
+	      .then(function successCallback(response) {
+	        markerData = response.data
+	      }, function errorCallback(response) {
+	      });
+	    }
+
 	    this.geocode = function() {
 	      for (var i = 0; i < markerData.length; i++){
 	        var geocoder = new google.maps.Geocoder();
@@ -31018,6 +31014,7 @@
 	        })
 	      }
 	    }
+	    
 	    this.initMap = function() {
 	      var mapDiv = document.getElementById('map');
 	      var map = new google.maps.Map(mapDiv, {
@@ -31034,76 +31031,7 @@
 	        });
 	      }
 	    }
-	  })
-
-
-
-
-
-
-
-	//   var marker = new google.maps.Marker({
-	//     map: map,
-	//     position: results[0].geometry.location
-	//   });
-	//   var infowindow = new google.maps.InfoW
-	// });
-	//
-	// var address1 = newAddress;
-	// createMarker(address1);
-	//
-	//
-	//
-	//
-	//
-	//
-	//     this.initMap = function() {
-	//       var mapDiv = document.getElementById('map');
-	//       var map = new google.maps.Map(mapDiv, {
-	//         center: {lat: 47.629, lng: -122.211},
-	//           zoom: 12
-	//       });
-	//       var geocoder = new google.maps.Geocoder();
-	//       var address = "1269 110th AVE NE Bellevue, WA 98004"
-	//       geocoder.geocode({'address': address}, function(results, status) {
-	//         var marker = new google.maps.Marker({
-	//           map: map,
-	//           position: results[0].geometry.location
-	//         });
-	//         var infowindow = new google.maps.InfoWindow({
-	//           content: '<p>Marker Location:' + marker.getPosition() + '</p>'
-	//         });
-	//       });
-	//
-	//     }
-	//   })
-
-
-	  // 'use strict';
-	  //
-	  // angular.module('MapModule', [])
-	  //   .controller('MapController', function() {
-	  //     this.initMap = function() {
-	  //       var mapDiv = document.getElementById('map');
-	  //       var map = new google.maps.Map(mapDiv, {
-	  //         center: {lat: 47.629, lng: -122.211},
-	  //           zoom: 12
-	  //       });
-	  //       var geocoder = new google.maps.Geocoder();
-	  //       var address = "1269 110th AVE NE Bellevue, WA 98004"
-	  //       geocoder.geocode({'address': address}, function(results, status) {
-	  //         console.log(results[0].geometry.location)
-	  //         var marker = new google.maps.Marker({
-	  //           map: map,
-	  //           position: results[0].geometry.location
-	  //         });
-	  //         var infowindow = new google.maps.InfoWindow({
-	  //           content: '<p>Marker Location:' + marker.getPosition() + '</p>'
-	  //         });
-	  //       });
-	  //
-	  //     }
-	  //   })
+	  }])
 
 
 /***/ },
