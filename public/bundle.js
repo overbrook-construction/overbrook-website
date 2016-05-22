6463fa9a74e05f60478832e79e8e33c61f950da3
Sam Gruse <sgruse89@gmail.com>
2016-05-21 19:58:53 -0700

    Thumbnail click/load home functionality is complete

--- public/bundle.js
+++ public/bundle.js
@@ -31112,39 +31112,27 @@
 
 	angular.module('GalleryModule', ['AjaxService'])
 	  .controller('GalleryController', ['$location', 'ajax', function($location, ajax) {
-	    // this.imgSrc = ['./media/630-001.jpg', './media/2432-001.jpg', './media/2434-001.jpg', './media/7720-001.jpg', './media/7728-001.jpg', './media/8102-001.jpg'];
 
-	  // this.imgSrc = [ajax.allHomeData.twoFourThreeTwo.pics[0],
-	  //                ajax.allHomeData.twoFourThreeFour.pics[0],
-	  //                ajax.allHomeData.sevenSevenTwoZero.pics[0]
-	  // ]
-
+	  this.showInfo = false;
 	  this.houseData = ajax.allHomeData;
 
-	  console.log('HOUSE DATA : ', this.houseData);
-
-
-	    // this.imgSrc = ajax.allHomeData.completed.twoFourThreeTwo.pics
-	    // console.log(this.jsonImage = ajax.allHomeData.completed.lucy.pics[0])
-
-	    // this.imgSrc = './media/630-001.jpg';
-
-
+	  this.singleHomeData = {};
 	    this.showInfoView = function() {
 	      $location.path('/info');
 	    }
 
-	    this.showClickedHome = function(clickedHome) {
-	      console.log('CLICKED HOME FUNCTION HAS BEEN HIT WITH : ', clickedHome);
-	      // Some function that grabs appropriate json data based on id
-	      var shd = {};
-	      shd.address = ajax.allHomeData.completed.clickedHome.address;
-	      return shd;
+	    this.singleHouseDataLoader = function(key){
+	      this.singleHomeData.address = ajax.allHomeData[key].address;
+	      this.singleHomeData.sqft = ajax.allHomeData[key].sqft;
+	      this.singleHomeData.bedrooms = ajax.allHomeData[key].bedrooms;
+	      this.singleHomeData.bathrooms = ajax.allHomeData[key].bathrooms;
+	      this.singleHomeData.lotsize = ajax.allHomeData[key].lotsize;
+	      this.singleHomeData.schooldistrict = ajax.allHomeData[key].schooldistrict;
+	      this.singleHomeData.status = ajax.allHomeData[key].status;
+	      this.singleHomeData.pics = ajax.allHomeData[key].pics;
+	      this.singleHomeData.mapPic = ajax.allHomeData[key].mapPic;
+	      this.singleHomeData.frontPic = ajax.allHomeData[key].pics[0];
 	    }
-
-	    this.info = 'dog';
-
-	    console.log('INFO VIEW AJAX SERVICE DATA : ', ajax.allHomeData);
 
 	  }]);
 
