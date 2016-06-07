98267667542e04b72b411451cb6f3d873bb5f237
Sam Gruse <sgruse89@gmail.com>
2016-06-07 13:33:36 -0700

    lskdjfdlsfj

--- app/components/gallery/gallery-controller.js
+++ app/components/gallery/gallery-controller.js
@@ -63,7 +63,7 @@ angular.module('GalleryModule', ['AjaxService'])
     }
 
     vm.runSingleData = function(id) {
-      console.log('MAKING CHANGES TO CLIENT SERVER !!!!!!!')
+      console.log('MAKING CHANGES TO CLIENT SERVER !!!!!!!!!!')
       vm.singleHouseDataLoader(id);
     }
 
